e694862b93243dd6d4a646d72319be8b06626b19
Persigio <sparkraven@gmail.com>
2021-02-09 13:06:44 -0500

    Created components

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Track.css';
+
+export class Track extends React.Component {
+    renderAction(){
+        let icon;
+        if(isRemoval){
+            icon = "-";
+        } else {
+            icon = "+";
+        }
+    }
+
+    render(){
+        return (
+            <div className="Track">
+            <div className="Track-information">
+                <h3><!-- track name will go here --></h3>
+                <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+            </div>
+            <button className="Track-action">{this.renderAction}</button>
+            </div>
+        );
+    }
+}
